51b192bf6ee84e93e642d498d195db2adf1f971f
Muhammed Yasin Özdemir <cozdemir56.yz@gmail.com>
2024-05-01 19:40:58 +0300

    Register FirebaseService as a singleton and add controllers

--- SoframiPaylas.WebAPI/Program.cs
+++ SoframiPaylas.WebAPI/Program.cs
@@ -1,10 +1,13 @@
+using SoframiPaylas.Infrastructure.Data.Service;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddSingleton<FirebaseService>();  // Firebase servisini singleton olarak kaydet
+builder.Services.AddControllers();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -23,7 +26,7 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
