7aef37d5378426d2dbfa047410d041f1548bfb41
Muhammed Yasin Özdemir <cavci48.yz@gmail.com>
2024-05-05 00:16:11 +0300

    feat(posts): update post actions and enhance DTOs with detailed documentation
    Implemented CRUD operations for posts with detailed Swagger annotations. Enhanced CreatePostDto, UpdatePostDto, and PostDto classes with clear field descriptions and realistic examples. Added error handling for not found and validation errors in post management actions to improve API robustness and developer experience.

--- SoframiPaylas.Application/DTOs/Post/PostDto.cs
+++ SoframiPaylas.Application/DTOs/Post/PostDto.cs
@@ -10,26 +10,70 @@ namespace SoframiPaylas.Application.DTOs.Post
 {
     public class PostDto
     {
-
+        /// <summary>
+        /// Gönderiyi oluşturan kullanıcının ID'si.
+        /// </summary>
+        /// <example>12345</example>
         public string HostID { get; set; }
+
+        /// <summary>
+        /// Gönderinin başlığı.
+        /// </summary>
+        /// <example>Yemek Paylaşım Etkinliği</example>
         public string Title { get; set; }
 
+        /// <summary>
+        /// Gönderi açıklaması.
+        /// </summary>
+        /// <example>Bu etkinlikte ev yapımı yemeklerimizi paylaşıyoruz!</example>
         public string Description { get; set; }
 
+        /// <summary>
+        /// Gönderinin gerçekleşeceği lokasyon.
+        /// </summary>
+        /// <example>{ Latitude = 41.0082, Longitude = 28.9784 }</example>
         public GeoPoint Location { get; set; }
 
+        /// <summary>
+        /// Gönderinin tarih bilgisi.
+        /// </summary>
+        /// <example>23/04/2024</example>
         public string FormattedDate { get; set; }
 
+        /// <summary>
+        /// Etkinliğin başlama saati.
+        /// </summary>
+        /// <example>18:30</example>
         public string Time { get; set; }
 
+        /// <summary>
+        /// Maksimum katılımcı sayısı.
+        /// </summary>
+        /// <example>20</example>
         public int MaxParticipants { get; set; }
+
+        /// <summary>
+        /// Gönderi ile ilişkili resimlerin listesi.
+        /// </summary>
+        /// <example>["https://example.com/images/event1.jpg", "https://example.com/images/event2.jpg"]</example>
         public List<string> Images { get; set; }
 
+        /// <summary>
+        /// Gönderinin aktif olup olmadığını gösterir.
+        /// </summary>
+        /// <example>true</example>
         public bool PostStatus { get; set; }
 
+        /// <summary>
+        /// Gönderi ile ilişkili yemeklerin listesi.
+        /// </summary>
+        /// <example>["Lahmacun", "Baklava"]</example>
         public List<string> RelatedFoods { get; set; }
 
-
+        /// <summary>
+        /// Etkinliğe katılan kullanıcıların listesi.
+        /// </summary>
+        /// <example>["user1", "user2"]</example>
         public List<string> Participants { get; set; }
     }
 }
--- SoframiPaylas.WebAPI/Controllers/PostController.cs
+++ SoframiPaylas.WebAPI/Controllers/PostController.cs
@@ -23,6 +23,41 @@ namespace SoframiPaylas.WebAPI.Controllers
             _postservice = postService;
             _participantService = participantService;
         }
+        /// <summary>
+        /// Sistemdeki tüm gönderileri listeler.
+        /// </summary>
+        /// <remarks>
+        /// Bu işlem, sistemde kaydedilmiş tüm aktif gönderileri getirir. Eğer gönderi bulunamazsa, 404 Not Found yanıtı döner.
+        ///
+        /// ### Örnek Yanıt
+        /// 
+        /// Başarılı yanıt aşağıdaki gibi olacaktır:
+        ///
+        ///     [
+        ///         {
+        ///             "hostID": "12345",
+        ///             "title": "Yemek Paylaşım Etkinliği",
+        ///             "description": "Bu etkinlikte ev yapımı yemeklerimizi paylaşıyoruz!",
+        ///             "location": { "latitude": 41.0082, "longitude": 28.9784 },
+        ///             "formattedDate": "23/04/2024",
+        ///             "time": "18:30",
+        ///             "maxParticipants": 20,
+        ///             "images": ["https://example.com/images/event1.jpg"],
+        ///             "postStatus": true,
+        ///             "relatedFoods": ["Lahmacun", "Baklava"],
+        ///             "participants": ["user1", "user2"]
+        ///         }
+        ///     ]
+        ///
+        /// ### Hatalar
+        /// 
+        /// - **404 Not Found**: Kayıtlı gönderi bulunamadığında bu hata dönülür.
+        /// - **500 Internal Server Error**: Gönderiler getirilirken beklenmedik bir hata oluşursa, bu hata dönülür.
+        /// </remarks>
+        /// <returns>Bir HTTP yanıtı döner ki bu, başarılı olursa gönderi listesini, başarısız olursa hata mesajını içerir.</returns>
+        /// <response code="200">Gönderiler başarıyla bulundu ve döndürüldü.</response>
+        /// <response code="404">Hiç gönderi bulunamadı.</response>
+        /// <response code="500">Gönderileri getirirken bir hata meydana geldi.</response>
         [HttpGet("posts")]
         public async Task<IActionResult> GetAllPostAsync()
         {
@@ -30,15 +65,56 @@ namespace SoframiPaylas.WebAPI.Controllers
             {
                 var posts = await _postservice.GetAllPostsAsync();
                 if (posts == null || !posts.Any())
-                    return NotFound("Post not found");
+                    return NotFound("Hiç gönderi bulunamadı.");
                 return Ok(posts);
             }
             catch (Exception ex)
             {
                 // Log the exception details here
-                return StatusCode(500, "An error occurred while deleting the post.");
+                return StatusCode(500, "Gönderileri getirirken bir hata meydana geldi.");
             }
         }
+        /// <summary>
+        /// Belirtilen ID'ye sahip gönderiyi getirir.
+        /// </summary>
+        /// <remarks>
+        /// Bu işlem, sistemde belirtilen ID'ye sahip gönderiyi arar ve bulursa döndürür. Eğer ilgili gönderi bulunamazsa, kullanıcıya 404 Not Found hatası ile bilgi verilir.
+        ///
+        /// ### Örnek İstek
+        /// 
+        ///     GET /api/posts/post/{postId}
+        ///     {
+        ///         "postId": "abc123"
+        ///     }
+        ///
+        /// ### Örnek Yanıt
+        /// 
+        /// Başarılı yanıt aşağıdaki gibi olacaktır:
+        ///
+        ///     {
+        ///         "hostID": "12345",
+        ///         "title": "Kültürlerarası Yemek Festivali",
+        ///         "description": "Dünya mutfaklarından lezzetler bu etkinlikte sizleri bekliyor!",
+        ///         "location": { "latitude": 41.0082, "longitude": 28.9784 },
+        ///         "formattedDate": "30/05/2024",
+        ///         "time": "14:00",
+        ///         "maxParticipants": 50,
+        ///         "images": ["https://example.com/images/festival.jpg"],
+        ///         "postStatus": true,
+        ///         "relatedFoods": ["Sushi", "Taco"],
+        ///         "participants": ["user1", "user3", "user5"]
+        ///     }
+        ///
+        /// ### Hatalar
+        /// 
+        /// - **404 Not Found**: Belirtilen ID'ye sahip gönderi bulunamazsa bu hata dönülür.
+        /// - **500 Internal Server Error**: Gönderi bilgisi getirilirken beklenmedik bir hata oluşursa, bu hata dönülür.
+        /// </remarks>
+        /// <param name="postId">Getirilecek gönderinin benzersiz tanımlayıcısı.</param>
+        /// <returns>HTTP yanıtı olarak, bulunan gönderi nesnesini veya hata mesajını içerir.</returns>
+        /// <response code="200">Gönderi başarıyla bulundu ve döndürüldü.</response>
+        /// <response code="404">Belirtilen ID'ye sahip gönderi bulunamadı.</response>
+        /// <response code="500">Gönderi bilgisi getirilirken bir hata meydana geldi.</response>
         [HttpGet("post/{postId}")]
         public async Task<IActionResult> GetPostByIdAsync(string postId)
         {
@@ -46,14 +122,47 @@ namespace SoframiPaylas.WebAPI.Controllers
             {
                 var post = await _postservice.GetPostByIdAsync(postId);
                 if (post == null)
-                    return NotFound("Post not found");
+                    return NotFound("Belirtilen ID'ye sahip gönderi bulunamadı.");
                 return Ok(post);
             }
             catch (Exception ex)
             {
-                return StatusCode(500, "An error occurred while deleting the post.");
+                return StatusCode(500, "Gönderi bilgisi getirilirken bir hata meydana geldi.");
             }
         }
+        /// <summary>
+        /// Yeni bir gönderi oluşturur.
+        /// </summary>
+        /// <remarks>
+        /// Bu işlem, gönderilen gönderi bilgileriyle yeni bir gönderi kaydı oluşturur. Eğer gönderi bilgileri doğrulanamazsa veya eksikse, 400 Bad Request hatası döner. Gönderi başarıyla oluşturulursa, gönderinin benzersiz tanımlayıcısı döndürülür.
+        ///
+        /// ### Örnek İstek
+        /// 
+        ///     POST /api/posts/post
+        ///     {
+        ///        "hostID": "12345",
+        ///        "title": "Uluslararası Mutfak Festivali",
+        ///        "description": "Dünya mutfaklarının en seçkin lezzetlerini deneyimleyin!",
+        ///        "location": { "latitude": 40.7128, "longitude": -74.0060 },
+        ///        "formattedDate": "01/10/2024",
+        ///        "time": "12:00",
+        ///        "maxParticipants": 100,
+        ///        "images": ["https://example.com/images/festival1.jpg", "https://example.com/images/festival2.jpg"],
+        ///        "postStatus": true,
+        ///        "relatedFoods": ["Sushi", "Taco", "Pizza"],
+        ///        "participants": ["user1", "user2"]
+        ///     }
+        ///
+        /// ### Hatalar
+        /// 
+        /// - **400 Bad Request**: Gönderilen verilerin doğrulaması başarısız olduğunda veya gerekli alanlar eksikse bu hata kodu döner.
+        /// - **500 Internal Server Error**: Gönderi oluşturma işlemi sırasında beklenmedik bir hata oluşursa, bu hata dönülür.
+        /// </remarks>
+        /// <param name="postDto">Oluşturulacak gönderinin bilgilerini içeren DTO nesnesi. DTO, 'hostID', 'title', 'description', 'location', 'formattedDate', 'time', 'maxParticipants', 'images', 'postStatus', 'relatedFoods', ve 'participants' alanlarını içermelidir.</param>
+        /// <returns>HTTP yanıtı olarak, başarılı ise gönderinin ID'si, başarısız ise hata mesajı içerir.</returns>
+        /// <response code="200">Gönderi başarıyla oluşturuldu ve gönderinin ID'si döndürüldü.</response>
+        /// <response code="400">Gönderilen verilerin doğrulaması başarısız olduğunda veya gerekli alanlar eksikse bu hata kodu döner.</response>
+        /// <response code="500">Gönderi oluşturma işlemi sırasında beklenmedik bir hata oluştuğunda bu hata dönülür.</response>
         [HttpPost("post")]
         public async Task<IActionResult> CreatePost([FromBody] CreatePostDto postDto)
         {
@@ -63,15 +172,51 @@ namespace SoframiPaylas.WebAPI.Controllers
                     return BadRequest(ModelState);
                 var postId = await _postservice.CreatePostAsync(postDto);
                 if (postId == null)
-                    return BadRequest("Failed to create post.");
+                    return BadRequest("Gönderilen verilerin doğrulaması başarısız olduğunda veya gerekli alanlar eksikse bu hata kodu döner.");
                 return Ok(postId);
             }
             catch (Exception ex)
             {
-                return StatusCode(500, "An error occurred while deleting the post.");
+                return StatusCode(500, "Gönderi oluşturma işlemi sırasında beklenmedik bir hata oluştuğunda bu hata dönülür.");
             }
         }
-
+        /// <summary>
+        /// Belirtilen ID'ye sahip gönderiyi günceller.
+        /// </summary>
+        /// <remarks>
+        /// Bu işlem, belirtilen ID'ye sahip gönderinin bilgilerini günceller. Gönderi bilgileri doğrulanamazsa 400 Bad Request hatası döner. Eğer belirtilen ID'ye sahip gönderi bulunamazsa 404 Not Found hatası döner. Güncelleme başarılı olursa, 200 OK yanıtı ile cevap döner.
+        ///
+        /// ### Örnek İstek
+        /// 
+        ///     PUT /api/posts/post
+        ///     {
+        ///        "postId": "abc123",
+        ///        "hostID": "12345",
+        ///        "title": "Kültürel Yemek Festivali Güncellendi",
+        ///        "description": "Festivalde yeni yemekler eklendi!",
+        ///        "location": { "latitude": 40.7128, "longitude": -74.0060 },
+        ///        "formattedDate": "05/10/2024",
+        ///        "time": "17:00",
+        ///        "maxParticipants": 150,
+        ///        "images": ["https://example.com/images/new_festival.jpg"],
+        ///        "postStatus": true,
+        ///        "relatedFoods": ["Ramen", "Taco", "Pasta"],
+        ///        "participants": ["user1", "user2", "user3"]
+        ///     }
+        ///
+        /// ### Hatalar
+        /// 
+        /// - **400 Bad Request**: Gönderilen verilerin doğrulaması başarısız olduğunda bu hata kodu döner.
+        /// - **404 Not Found**: Belirtilen ID'ye sahip gönderi bulunamazsa bu hata dönülür.
+        /// - **500 Internal Server Error**: Gönderi güncelleme işlemi sırasında beklenmedik bir hata oluşursa, bu hata dönülür.
+        /// </remarks>
+        /// <param name="postDto">Güncellenmek istenen gönderinin bilgilerini içeren DTO nesnesi.</param>
+        /// <param name="postId">Güncellenmek istenen gönderinin benzersiz tanımlayıcısı.</param>
+        /// <returns>HTTP yanıtı olarak, başarılı ise boş bir cevap, başarısız ise hata mesajı içerir.</returns>
+        /// <response code="200">Gönderi başarıyla güncellendi.</response>
+        /// <response code="400">Gönderilen verilerin doğrulaması başarısız olduğunda bu hata kodu döner.</response>
+        /// <response code="404">Belirtilen ID'ye sahip gönderi bulunamadı.</response>
+        /// <response code="500">Gönderi güncelleme işlemi sırasında beklenmedik bir hata oluştuğunda bu hata döner.</response>
         [HttpPut("post")]
         public async Task<IActionResult> UpdatePost([FromBody] UpdatePostDto postDto, string postId)
         {
@@ -81,12 +226,12 @@ namespace SoframiPaylas.WebAPI.Controllers
                     return BadRequest(ModelState);
                 var updateResult = await _postservice.UpdatePostAsync(postId, postDto);
                 if (!updateResult)
-                    return NotFound("Post to update not found.");
+                    return NotFound("Belirtilen ID'ye sahip gönderi bulunamadı.");
                 return Ok();
             }
             catch (Exception ex)
             {
-                return StatusCode(500, "An error occurred while deleting the post.");
+                return StatusCode(500, "Gönderi güncelleme işlemi sırasında beklenmedik bir hata oluştuğunda bu hata döner.");
             }
         }
 
@@ -105,7 +250,31 @@ namespace SoframiPaylas.WebAPI.Controllers
                 return StatusCode(500, "An error occurred while deleting the post.");
             }
         }
-
+        /// <summary>
+        /// Belirtilen ID'ye sahip gönderiyi sistemden siler.
+        /// </summary>
+        /// <remarks>
+        /// Bu işlem, sistemde belirtilen ID'ye sahip gönderiyi arar ve siler. Gönderi başarıyla silinirse 204 No Content yanıtı döner. 
+        /// Eğer belirtilen ID'ye sahip gönderi bulunamazsa, 404 Not Found hatası döner.
+        ///
+        /// ### Örnek İstek
+        /// 
+        ///     DELETE /api/posts/post
+        ///     {
+        ///         "postId": "abc123"
+        ///     }
+        ///
+        /// ### Hatalar
+        /// 
+        /// - **204 No Content**: Gönderi başarıyla silindi, hiçbir içerik dönmez.
+        /// - **404 Not Found**: Belirtilen ID'ye sahip gönderi bulunamazsa bu hata dönülür.
+        /// - **500 Internal Server Error**: Gönderi silme işlemi sırasında beklenmedik bir hata oluşursa, bu hata dönülür.
+        /// </remarks>
+        /// <param name="postId">Silinmek istenen gönderinin benzersiz tanımlayıcısı.</param>
+        /// <returns>HTTP yanıtı olarak, başarılı ise içerik dönmeyen yanıt, başarısız ise hata mesajı içerir.</returns>
+        /// <response code="204">Gönderi başarıyla silindi, içerik dönmez.</response>
+        /// <response code="404">Belirtilen ID'ye sahip gönderi bulunamadı.</response>
+        /// <response code="500">Gönderi silme işlemi sırasında beklenmedik bir hata oluştuğunda bu hata dönülür.</response>
         [HttpPost("{postId}/join")]
         public async Task<IActionResult> RequestJoinEvent(string postId, [FromBody] JoinParticipantDto joinRequest)
         {
@@ -113,12 +282,12 @@ namespace SoframiPaylas.WebAPI.Controllers
             {
                 var joinResult = await _participantService.AddParticipantAsync(postId, joinRequest);
                 if (!joinResult)
-                    return BadRequest("Failed to send join request.");
-                return Ok(new { message = "Join request sent successfully, waiting for host approval." });
+                    return BadRequest("Belirtilen ID'ye sahip gönderi bulunamadı.");
+                return NoContent();
             }
             catch (Exception ex)
             {
-                return StatusCode(500, "An error occurred while deleting the post.");
+                return StatusCode(500, "Gönderi silme işlemi sırasında beklenmedik bir hata oluştuğunda bu hata dönülür.");
             }
         }
 
